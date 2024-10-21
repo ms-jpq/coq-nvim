0d3aa376fa10ac205ace6221583c3a5703b1e09f
dogisgreat <github@bigly.dog>
2024-10-20 23:47:58 -0400

    pragma

--- coq/clients/buffers/db/sql/create/pragma.sql
+++ coq/clients/buffers/db/sql/create/pragma.sql
@@ -1,1 +1,3 @@
+PRAGMA auto_vacuum = INCREMENTAL;
 PRAGMA foreign_keys = ON;
+PRAGMA temp_store = MEMORY;
--- coq/clients/snippet/db/sql/create/pragma.sql
+++ coq/clients/snippet/db/sql/create/pragma.sql
@@ -1,2 +1,4 @@
+PRAGMA auto_vacuum = INCREMENTAL;
 PRAGMA foreign_keys = ON;
 PRAGMA journal_mode = WAL;
+PRAGMA temp_store = MEMORY;

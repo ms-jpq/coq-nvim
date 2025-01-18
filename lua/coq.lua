f518278fb2a6a72fe2c671e97e0cf320e62ed86d
dogisgreat <github@bigly.dog>
2025-01-18 01:03:14 -0500

    oops

--- lua/coq.lua
+++ lua/coq.lua
@@ -2,7 +2,7 @@ COQ = COQ or {}
 coq = coq or {}
 
 coq.validate = (function()
-  if not vim.fn.has("nvim-0.11") then
+  if vim.fn.has("nvim-0.11") == 0 then
     return vim.validate
   else
     return function(spec)

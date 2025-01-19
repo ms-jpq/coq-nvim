5f7de592c0ed2fb79a90bce26bf5d33c238b6ff7
dogisgreat <git@bigly.dog>
2025-01-19 04:39:00 -0500

    beta dist

--- coq/clients/lsp/mul_bandit.py
+++ coq/clients/lsp/mul_bandit.py
@@ -1,8 +1,22 @@
 from collections import defaultdict
 from datetime import timedelta
-from math import inf
-from random import random
+from math import exp, gamma, inf, log
+from random import random, uniform
 from typing import AbstractSet, MutableSet, Optional
+
+
+def _kumaraswamy_inv_cdf(y: float, alpha: float, beta: float) -> float:
+    """
+    https://en.wikipedia.org/wiki/Kumaraswamy_distribution
+    """
+
+    return (1 - (1 - y) ** (1 - beta)) ** (1 / alpha)
+
+
+def _random_variate_sampling(a: float, b: float) -> float:
+
+    u = uniform(0, 1)
+    return _kumaraswamy_inv_cdf(u, alpha=a, beta=b)
 
 
 class MultiArmedBandit:

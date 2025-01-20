329167db7d7b8787ab79277bdc324e624b49aaed
dogisgreat <git@bigly.dog>
2025-01-19 22:54:48 -0500

    binning
    oops

--- coq/clients/lsp/mul_bandit.py
+++ coq/clients/lsp/mul_bandit.py
@@ -1,22 +1,43 @@
+from bisect import bisect
 from collections import defaultdict
 from datetime import timedelta
-from math import exp, gamma, inf, log
+from math import exp, floor, gamma, inf, log
 from random import random, uniform
-from typing import AbstractSet, MutableSet, Optional
+from typing import AbstractSet, MutableSet, Optional, Sequence
+
+from std2.itertools import pairwise
 
 
-def _kumaraswamy_inv_cdf(y: float, alpha: float, beta: float) -> float:
-    """
-    https://en.wikipedia.org/wiki/Kumaraswamy_distribution
-    """
-
-    return (1 - (1 - y) ** (1 - beta)) ** (1 / alpha)
+def _logit(x: float) -> float:
+    return log(x / (1 - x))
 
 
-def _random_variate_sampling(a: float, b: float) -> float:
+def _bins(k: float, n: int) -> Sequence[float]:
+    return tuple((exp(k * i) - 1) * 1000 for i in range(n)) + (inf,)
 
-    u = uniform(0, 1)
-    return _kumaraswamy_inv_cdf(u, alpha=a, beta=b)
+
+class _Dist:
+    def __init__(self) -> None:
+        self._bins = _bins(k=0.03, n=6)
+        self._decay = 0.99
+        self._cdf = [0.0 for _ in pairwise(self._bins)]
+        self._sum = 0.0
+
+    def update(self, x: float) -> None:
+        binned, inc = False, 0.0
+        for i, (lo, hi) in enumerate(pairwise(self._bins)):
+            binned |= lo <= x < hi
+            inc += binned
+            self._cdf[i] = self._cdf[i] * self._decay + binned
+        self._cdf[-1] = self._cdf[-1] * self._decay + inc
+
+    def inv_cdf(self, p: float) -> float:
+        assert 0 <= p <= 1
+
+        i = round((len(self._cdf) - 1) * p)
+        print(self._cdf[i + 1])
+        frac = (p - self._cdf[i]) / (self._cdf[i + 1] - self._cdf[i])
+        return self._bins[i] + frac * (self._bins[i + 1] - self._bins[i])
 
 
 class MultiArmedBandit:
